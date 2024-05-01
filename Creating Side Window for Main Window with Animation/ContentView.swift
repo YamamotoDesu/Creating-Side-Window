9a549f40d44a480a680242ade0879f2e4b68797a
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2024-05-01 11:00:15 +0900

    Initial Commit

---
+++ Creating Side Window for Main Window with Animation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Creating Side Window for Main Window with Animation
+//
+//  Created by Yamamoto Kyo on 2024/05/01.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Creating Side Window for Main Window with Animation/Creating_Side_Window_for_Main_Window_with_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Creating_Side_Window_for_Main_Window_with_AnimationApp.swift
+//  Creating Side Window for Main Window with Animation
+//
+//  Created by Yamamoto Kyo on 2024/05/01.
+//
+
+import SwiftUI
+
+@main
+struct Creating_Side_Window_for_Main_Window_with_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
